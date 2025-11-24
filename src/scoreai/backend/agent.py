f8b97331319972117e555eecdb39f16b4c525597
arnaudon <alexis.arnaudon@epfl.ch>
2025-11-24 22:19:07 +0100

    fix

--- src/scoreai/backend/agent.py
+++ src/scoreai/backend/agent.py
@@ -21,40 +21,42 @@ class Filter(BaseModel):
     composer: str
 
 
-agent = Agent(
-    MODEL,
-    output_type=Response,
-    deps_type=Scores,
-    system_prompt="""Your task it to find a good score to play.
+def get_agent():
+    agent = Agent(
+        MODEL,
+        output_type=Response,
+        deps_type=Scores,
+        system_prompt="""Your task it to find a good score to play.
         Write score id entry into score_id.
         If multiple scores are possible, return None for the score_id.
         If one score is available, write score_id.
         Do not mention score_id in your response.
         If multiple choices are possible, list them without id.
         """,
-)
+    )
 
+    @agent.tool
+    async def get_score_info(ctx: RunContext[Scores]) -> str:
+        """Get score info."""
+        return f"The scores infos are {ctx.deps.model_dump_json()}."
 
-@agent.tool
-async def get_score_info(ctx: RunContext[Scores]) -> str:
-    """Get score info."""
-    return f"The scores infos are {ctx.deps.model_dump_json()}."
+    @agent.tool
+    async def get_random_score_by_composer(ctx: RunContext[Scores], filter_params: Filter) -> str:
+        """Get a random score by composer."""
+        scores = []
+        for score in ctx.deps.scores:
+            if score.composer.lower() == filter_params.composer.lower():
+                scores.append(score)
+        if scores:
+            return random.choice(scores).model_dump_json()
+        return "Not found"  # pragma: no cover
 
-
-@agent.tool
-async def get_random_score_by_composer(ctx: RunContext[Scores], filter_params: Filter) -> str:
-    """Get a random score by composer."""
-    scores = []
-    for score in ctx.deps.scores:
-        if score.composer.lower() == filter_params.composer.lower():
-            scores.append(score)
-    if scores:
-        return random.choice(scores).model_dump_json()
-    return "Not found"  # pragma: no cover
+    return agent
 
 
 async def run_agent(prompt: str, deps: Scores, message_history=None):
     """Run the agent."""
+    agent = get_agent()
     res = await agent.run(
         prompt,
         message_history=message_history,
--- tests/backend/test_agent.py
+++ tests/backend/test_agent.py
@@ -2,13 +2,13 @@
 
 from pydantic_ai.models.test import TestModel
 
-from scoreai.backend.agent import agent, run_agent
+from scoreai.backend import agent
 from scoreai.shared_models.responses import FullResponse
 from scoreai.shared_models.scores import Scores
 
 
 async def test_agent(test_scores: Scores):
     """test agent"""
-    with agent.override(model=TestModel()):
-        result = await run_agent(prompt="test", deps=test_scores)
+    agent.MODEL = TestModel()
+    result = await agent.run_agent(prompt="test", deps=test_scores)
     assert isinstance(result, FullResponse)
