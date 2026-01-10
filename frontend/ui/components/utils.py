99837797978916241df8d63f386f03b8cba027e4
Alexis Arnaudon <alexis.arnaudon@gmail.com>
2026-01-10 17:53:01 +0100

    Add S3 secrets to CI (#12)
    Co-authored-by: arnaudon <alexis.arnaudon@epfl.ch>

--- frontend/ui/components/utils.py
+++ frontend/ui/components/utils.py
@@ -1,6 +1,7 @@
 """Utils."""
 
 import os
+
 import boto3
 from botocore.config import Config
 
@@ -15,7 +16,7 @@ def get_s3_helper() -> dict:
             endpoint_url="https://" + str(os.getenv("S3_ENDPOINT")),
             aws_access_key_id=os.getenv("S3_ACCESS_KEY"),
             aws_secret_access_key=os.getenv("S3_SECRET_KEY"),
-            region_name=os.getenv("S3_REGION"),
+            region_name=os.getenv("S3_REGION", "us-east-1"),
             config=Config(
                 signature_version="s3v4",
                 request_checksum_calculation="when_required",
