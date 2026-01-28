8d8ab4645a6b435ce4e1072d79c1ecb4ef3e772a
arnaudon <alexis.arnaudon@epfl.ch>
2026-01-28 16:10:57 +0100

    more

--- script/imslp_scrapping/test.py
+++ script/imslp_scrapping/test.py
@@ -1,32 +1,109 @@
 import requests
+import json
+from tqdm import tqdm
 from bs4 import BeautifulSoup
-from pprint import pprint
 import pandas as pd
+from sqlmodel import Field, SQLModel
+import os
+from sqlmodel import Session, create_engine
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///imslp.db")
+bypass_metadata = False
 
 
-def get_works():
+class IMSLPEntry(SQLModel, table=True):
+    """IMSLPEntry model."""
+
+    id: int | None = Field(default=None, primary_key=True)
+    title: str = Field()
+    composer: str = Field()
+    permlink: str = Field()
+    score_metadata: str = Field(default="")
+    pdf_urls: str = Field(default="")
+
+
+def init_db():
+    """Initialize database."""
+    engine = create_engine(DATABASE_URL)
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    """Get database session."""
+    engine = create_engine(
+        DATABASE_URL,
+        pool_size=5,
+        max_overflow=10,
+        pool_recycle=3600,
+        pool_timeout=30,
+    )
+    with Session(engine) as session:
+        yield session
+
+
+def get_works(max_pages=2, work_per_page=10):
     """return a dataframe of works"""
     # TODO: do this once and save in a table
     df = pd.DataFrame()
-    for i in range(10):
+    gen_db = get_session()
+    session = next(gen_db)
+    for i in range(max_pages):
         print("loading page ", i)
         start = i * 1000
         url = f"https://imslp.org/imslpscripts/API.ISCR.php?account=worklist/disclaimer=accepted/sort=id/type=2/start={start}"
-        data = requests.get(url).json()
+        response = requests.get(url)
+        data = response.json()
         data.pop("metadata")
-        for i, item in data.items():
+        for i, item in tqdm(data.items()):
             i = int(i) + start
             df.loc[i, "title"] = item["intvals"]["worktitle"]
             df.loc[i, "composer"] = item["intvals"]["composer"]
             df.loc[i, "permlink"] = item["permlink"]
+            if not bypass_metadata:
+                response = requests.get(item["permlink"])
+                score_metadata = get_metadata(response, bypass=bypass_metadata)
+                pdf_urls = json.dumps(get_pdfs(response))
+                df.loc[i, "score_metadata"] = score_metadata
+                df.loc[i, "pdf_urls"] = pdf_urls
+            entry = IMSLPEntry(
+                id=i,
+                title=item["intvals"]["worktitle"],
+                composer=item["intvals"]["composer"],
+                permlink=item["permlink"],
+                score_metadata=score_metadata,
+                pdf_urls=pdf_urls,
+            )
+            session.add(entry)
+            if i > work_per_page - 1 + start:
+                break
+        session.commit()
+    try:
+        next(gen_db)
+    except StopIteration:
+        pass
     return df
 
 
-def get_pdfs(url):
+def get_metadata(response, bypass=False):
+    """return a dictionary of metadata from the page"""
+    if bypass:
+        return ""
+    soup = BeautifulSoup(response.text, "html.parser")
+    table = soup.find("span", id="General_Information").find_next("table")
+    data = {}
+
+    for row in table.find_all("tr"):
+        header = row.find("th")
+        value = row.find("td")
+        if header and value:
+            key = header.get_text(" ", strip=True)
+            val = value.get_text(" ", strip=True)
+            data[key] = val
+    return json.dumps(data)
+
+
+def get_pdfs(response):
     """return a list of pdf urls"""
-    # TODO: show a little dialog to compare pdfs and see which one is better before downloading
-    # first from the landing page we go to the pdf page
-    response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     links = soup.find_all("a", href=True)
     pdf_landing_pages = [
@@ -43,13 +120,27 @@ def get_pdfs(url):
 
         soup = BeautifulSoup(response.text, "html.parser")
         links = soup.find_all("span", id="sm_dl_wait")
-        pdf_urls += [l["data-id"] for l in links]
+        if links:
+            pdf_urls += [l["data-id"] for l in links]
+        # try redirect
+        else:
+            response = session.head(
+                str(pdf_landing_page), cookies=cookies, allow_redirects=True
+            )
+            pdf_url = response.url
+            if pdf_url.endswith("pdf"):
+                pdf_urls.append(pdf_url)
+            else:
+                print(pdf_url)
+    print(pdf_urls)
     return pdf_urls
 
 
 if __name__ == "__main__":
+    init_db()
     df = get_works()
     print(df)
+    df.to_csv("imslp.csv")
     url = df.loc[10, "permlink"]
     pdf_urls = get_pdfs(url)
     print(pdf_urls)
